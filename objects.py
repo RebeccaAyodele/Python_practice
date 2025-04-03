0861ee86805f99b8cd8dd1cff72fad9774cd32d8
Rebecca Ayodele <rebeccaayodele08@gmail.com>
2025-04-03 23:45:15 +0100

    Practice

--- objects.py
+++ objects.py
@@ -1,9 +1,13 @@
 phone = input ("Phone: ")
 
-phone = {
+digits = {
     "1": "one",
     "2": "two",
     "3": "three",
     "4": "four",
-
 }
+
+output = ""
+for i in phone:
+    output += digits.get(i, "!") + " "
+print(output)
